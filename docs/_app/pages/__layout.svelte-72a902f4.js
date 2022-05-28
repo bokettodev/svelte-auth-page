c78ae0eef96238de8f5b73b8f0c3c11bb6e98bc0
Raphael Jancherov <bokettodev@gmail.com>
2022-05-29 00:14:51 +0300

    chore(gh-pages): gh-pages added

---
+++ docs/_app/pages/__layout.svelte-72a902f4.js
@@ -0,0 +1,141 @@
+import{S as h,i as d,s as y,F as p,e as u,t as g,k as b,G as _,c as v,a as z,h as k,d as f,m as x,b as $,H as m,g as w,I as L,J as R,K as S,r as E,p as T}from"../chunks/index-ef15abbd.js";function q(r){let t,l,s,a;const i=r[1].default,o=p(i,r,r[0],null);return{c(){t=u("style"),l=g(`/**
+ * The application's typography styles.
+ */
+@import url("https://fonts.googleapis.com/css2?family=Roboto");
+@import url("https://fonts.googleapis.com/css2?family=Lora");
+:root {
+  --font-family-primary: "Roboto", arial, helvetica, sans-serif;
+  --font-family-secondary: "Lora", arial, helvetica, sans-serif;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: var(--font-family-secondary);
+  font-weight: bold;
+}
+
+h1 {
+  font-size: 3rem;
+}
+
+h2 {
+  font-size: 2.5rem;
+}
+
+h3 {
+  font-size: 2rem;
+}
+
+h4 {
+  font-size: 1.5rem;
+}
+
+h5 {
+  font-size: 1.25rem;
+}
+
+h6 {
+  font-size: 1rem;
+}
+
+:root {
+  --brand-color: #2320a5;
+  --accent-color: #fd767a;
+  --background-primary-color: #ffffff;
+  --background-secondary-color: #e5e5e5;
+  --background-tertiary-color: #f2f1fa;
+  --text-primary-color: #57575f;
+  --text-primary-invert-color: #fbfbfe;
+  --text-secondary-color: #a9a2d4;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+*:focus {
+  outline: none;
+}
+
+body {
+  width: 100vw;
+  height: 100vh;
+  font-family: var(--font-family-primary);
+  color: var(--text-primary-color);
+  background-color: var(--background-tertiary-color);
+}`),s=b(),o&&o.c(),this.h()},l(n){const e=_('[data-svelte="svelte-ryoav3"]',document.head);t=v(e,"STYLE",{lang:!0});var c=z(t);l=k(c,`/**
+ * The application's typography styles.
+ */
+@import url("https://fonts.googleapis.com/css2?family=Roboto");
+@import url("https://fonts.googleapis.com/css2?family=Lora");
+:root {
+  --font-family-primary: "Roboto", arial, helvetica, sans-serif;
+  --font-family-secondary: "Lora", arial, helvetica, sans-serif;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: var(--font-family-secondary);
+  font-weight: bold;
+}
+
+h1 {
+  font-size: 3rem;
+}
+
+h2 {
+  font-size: 2.5rem;
+}
+
+h3 {
+  font-size: 2rem;
+}
+
+h4 {
+  font-size: 1.5rem;
+}
+
+h5 {
+  font-size: 1.25rem;
+}
+
+h6 {
+  font-size: 1rem;
+}
+
+:root {
+  --brand-color: #2320a5;
+  --accent-color: #fd767a;
+  --background-primary-color: #ffffff;
+  --background-secondary-color: #e5e5e5;
+  --background-tertiary-color: #f2f1fa;
+  --text-primary-color: #57575f;
+  --text-primary-invert-color: #fbfbfe;
+  --text-secondary-color: #a9a2d4;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+*:focus {
+  outline: none;
+}
+
+body {
+  width: 100vw;
+  height: 100vh;
+  font-family: var(--font-family-primary);
+  color: var(--text-primary-color);
+  background-color: var(--background-tertiary-color);
+}`),c.forEach(f),e.forEach(f),s=x(n),o&&o.l(n),this.h()},h(){document.title="Svelte Auth Page",$(t,"lang","scss")},m(n,e){m(document.head,t),m(t,l),w(n,s,e),o&&o.m(n,e),a=!0},p(n,[e]){o&&o.p&&(!a||e&1)&&L(o,i,n,n[0],a?S(i,n[0],e,null):R(n[0]),null)},i(n){a||(E(o,n),a=!0)},o(n){T(o,n),a=!1},d(n){f(t),n&&f(s),o&&o.d(n)}}}function A(r,t,l){let{$$slots:s={},$$scope:a}=t;return r.$$set=i=>{"$$scope"in i&&l(0,a=i.$$scope)},[a,s]}class F extends h{constructor(t){super(),d(this,t,A,q,y,{})}}export{F as default};
